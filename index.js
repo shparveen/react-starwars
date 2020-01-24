056b89ee0a3a51851519870206748c28f5eb5cb5
shparveen <shparmeen@gmail.com>
2020-01-24 19:16:11 +0530

    2 files modified

--- index.js
+++ index.js
@@ -4,36 +4,66 @@ import Hello from './Hello';
 import './style.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'StarWars'
-    };
+   state = {
+      loading:true,
+      person:null,
+      users:null
   }
 
+  componentDidMount(){
+    const url = "https://api.randomuser.me/";
+     fetch("https://api.randomuser.me/")
+     .then(response => response.json())
+      .then(data => {
+        console.log(data.results[0])
+          this.setState({ person:data.results[0], loading:false})
+        }
+        
+      );
+  
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response =>response.json())
+    .then(data =>{
+        console.log(data);
+         this.setState({ users:data, loading:false})
+    })
+  }
   render() {
+    if(this.state.loading ){
+      return <div>loading..</div>
+    }
+    if(!this.state.person){
+      return <div>Person is not present</div>
+    }
+    if(!this.state.users){
+      return <div>Users is not present</div>
+    }
+    
+
+
+const {loading,person,users} = this.state
     return (
       <div>
-        <Hello name={this.state.name} />   
-        <div class="login-page">
-          <div class="form">
-            <form class="register-form">
-              <input type="text" placeholder="name"/>
-              <input type="password" placeholder="password"/>
-              <input type="text" placeholder="email address"/>
-              <button>create</button>
-              <p class="message">Already registered? <a href="#">Sign In</a></p>
-            </form>
-            <form class="login-form">
-              <input type="text" placeholder="username"/>
-              <input type="password" placeholder="password"/>
-              <button>login</button>
-              <p class="message">Not registered? <a href="#">Create an account</a></p>
-            </form>
-          </div>
-        </div>
+      <h1>Users</h1>
+      
+          {users.map(userdata=>(
+            <p> Name: {userdata.name} | email: {userdata.email}</p>
+          ))}
+      
+      <div>
+       <div>
+            <div>{person.name.title}</div>
+            <div>{person.name.first}</div>
+            <div>{person.name.last}</div>
+            <div><img src={person.picture.large} /></div>
+         </div>
+      </div>
       </div>
     );
+
+    
+    
+
   }
 }
 
